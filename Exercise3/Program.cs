53de828e2bd5805f25fd109315d2435803b9769d
Marshall <363752922@qq.com>
2021-06-14 18:26:50 -0400

    Ex3

---
+++ Exercise3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void f(ref string s)
+        {
+            s += "word";
+        }
+        static void Main(string[] args)
+        {
+            string s = "Hello";
+            f(ref s);
+            Console.WriteLine(s);
+
+        }
+    }
+}
