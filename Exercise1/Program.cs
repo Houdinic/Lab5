93b15554605b77b6d2f9819ec18f3c3645b39794
Marshall <363752922@qq.com>
2021-06-14 17:38:53 -0400

    Ex1

---
+++ Exercise1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var powers = new[] { 1, 2, 4,16,32,64,128,256,512,1024 };
+            foreach (var item in powers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
